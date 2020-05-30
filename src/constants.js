16d4142a1bc3e6718f14fdf009e11c24f3aca7aa
Gianpiero <gianpiero.costanza@gmail.com>
2020-05-31 00:37:04 +0200

    modify webpack.config.js and prettier

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const listTypes = {
+  "populares-en-taquilla": "populares-en-taquilla",
+  "estrenos-para-toda-la-familia": "estrenos-para-toda-la-familia",
+  "estrenos-imprescindibles-en-taquilla":
+    "estrenos-imprescindibles-en-taquilla",
+  "estrenos-espanoles": "estrenos-espanoles",
+  "si-te-perdiste": "si-te-perdiste",
+  "especial-x-men": "especial-x-men",
+  "nuestras-preferidas-de-la-semana": "nuestras-preferidas-de-la-semana",
+};
+
+export const rakutenQueryParams =
+  "?classification_id=5&device_identifier=web&locale=es&market_code=es";
+
+export const API_URL = "https://gizmo.rakuten.tv";
+export const MOVIE_ENDPOINT = "/v3/movies/";
+export const STREAMINGS_ENDPOINT = "/v3/streamings";
+export const LIST_ENDPOINT = "/v3/lists/";
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,26 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve("build"),
-    filename: "main.js"
+    path: path.join(__dirname, "build"),
+    filename: "main.js",
   },
+  mode: process.env.NODE_ENV || "development",
   module: {
     rules: [
-      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ }
-    ]
+      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
-  ]
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./src"),
+    port: 3000,
+    watchContentBase: true,
+    open: true,
+  },
 };
