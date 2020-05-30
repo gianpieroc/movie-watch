b65ef2d8671f1360e67c9245deed0859f138c799
Gianpiero <gianpiero.costanza@gmail.com>
2020-05-31 00:32:33 +0200

    fetch api tests

---
+++ src/utils/fetch-api.test.js
@@ -0,0 +1,32 @@
+import fetchApi from "./fetch-api";
+
+describe("fetchApi tests", () => {
+  const unMockedFetch = global.fetch;
+
+  beforeEach(() => {
+    global.fetch = () =>
+      Promise.resolve({
+        ok: true,
+        json: () => ({
+          data: "data"
+        })
+      });
+  });
+
+  afterEach(() => {
+    global.fetch = unMockedFetch;
+  });
+
+  it("should call fetchApi successfully", async () => {
+    const response = await fetchApi("https://www.rakuten.tv/");
+    expect(response.data).toEqual("data");
+  });
+
+  it("should throw error in fetchApi if ok is not true", async () => {
+    global.fetch = () => Promise.resolve({ ok: false });
+    const response = await fetchApi("https://www.rakuten.tv/");
+    expect(response.message).toEqual(
+      "Failed connecting to https://www.rakuten.tv/"
+    );
+  });
+});
