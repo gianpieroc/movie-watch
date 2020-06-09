56afb632e67ff774a4ce187f3bff3a24cbf8f0f1
Gianpiero <gianpiero.costanza@gmail.com>
2020-06-10 00:24:45 +0200

    theme for button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import theme from "../../globalStyles/theme";
 import Button from "./index";
 
 describe("HorizontalList", () => {
   it("Should render button", () => {
-    const { getByText } = render(<Button>Test</Button>);
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <Button>Test</Button>
+      </ThemeProvider>
+    );
     expect(getByText("Test")).toBeTruthy();
   });
 
   it("Should click button with a callback", () => {
     const onClick = jest.fn();
-    const { getByText } = render(<Button onClick={onClick}>Test</Button>);
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <Button onClick={onClick}>Test</Button>
+      </ThemeProvider>
+    );
     const button = getByText("Test");
     fireEvent.click(button);
     expect(onClick).toHaveBeenCalled();
